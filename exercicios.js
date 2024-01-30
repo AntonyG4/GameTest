984aefe4d220e642f665954d670fce736fe2e18d
AntonyG4 <58792161+AntonyG4@users.noreply.github.com>
2024-01-30 01:54:04 -0300

    Add files via upload

---
+++ exercicios.js
@@ -0,0 +1,8 @@
+//Lista generica
+//let listaGenerica = []; 
+
+let linguagensDeProgramação = ['JavaScript', 'C', 'C++', 'Kotlin', 'Python'];
+linguagensDeProgramação.push('Java', 'Ruby', 'GoLang');
+console.log(linguagensDeProgramação[0]);
+console.log(linguagensDeProgramação[1]);
+console.log(linguagensDeProgramação[linguagensDeProgramação.length - 1]);
